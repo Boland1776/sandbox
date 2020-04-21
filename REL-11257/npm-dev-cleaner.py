6978e1db0eb907040e942c59c8c457321da4369c
Boland1776 <Boland1776@gmail.com>
2020-04-21 13:39:26 -0400

    Fix print issue when VERBOSE not set

--- REL-11257/npm-dev-cleaner.py
+++ REL-11257/npm-dev-cleaner.py
@@ -233,7 +233,6 @@ def parse_options():
     tmp = os.getenv("VERBOSE")
     if tmp and tmp.lower() in ['true', '1']:
         VERBOSE = True
-        print 'VERBOSE is set in ENV'
 
     tmp = os.getenv("KEEP_FILES")
     if tmp and tmp.lower() in ['true', '1']:
@@ -281,8 +280,6 @@ def lprint(msg, wait):
         else:
             print msg               # Else, just print message
     else:
-        print 'lprint verbose not set'
-        print 'MSG: "%s"' % msg
         if re.match(r'\* Warning', msg, re.IGNORECASE):
             print msg
     sys.stdout.flush()              # One final flush for the rest
