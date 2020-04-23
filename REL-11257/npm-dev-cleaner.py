f868535165d083e4f9371a3d5e57f9bdebefca94
Boland1776 <Boland1776@gmail.com>
2020-04-23 07:36:57 -0400

    Add more comments Add Interactive mode Add DELETE_ONE

--- REL-11257/npm-dev-cleaner.py
+++ REL-11257/npm-dev-cleaner.py
@@ -13,17 +13,18 @@ import requests
 import sys
 import time
 
-# When set, we collect all the data via curl and save the data for future test runs. If the flag is False, we dont
-# run the "curl" and rely on the data saved in the files (initially, these curls are taking 10's of minutes).
+# When set, we collect all the data via curl and save that data for future test runs. If the flag is False, we dont
+# run the "curl" and rely on the data saved (initially, these curls are taking 10's of minutes).
 GEN_SAVED_DATA = True   # By default we poll and save the data (False when debugging and we catalog files saved)
 
 # User options (some are only available via CLI)
-CLEAN     = True     # Clean any files I create (except the log)
-VERBOSE   = False
-TEST_MODE = False    # Do not set when calling from pipeline
-WAIT      = False    # Wait for user if true.
-DO_DELETE = False    # Saftey measure. You MUST call script with "-D" to actually delete
-MAX_DAYS  = 30
+CLEAN     = True    # Clean any files I create (except the log) : CLI and Jenkins
+DELETE_ONE = False  # Delete on file and exit : CLI and Jenkins (for now)
+INTERACTIVE = False # Have user confirm deletion for each file (for debugging) : CLI
+VERBOSE   = False   # Show what's being done : CLI and Jenkins
+WAIT      = False   # Wait for user if true. : CLI
+DO_DELETE = False   # Saftey measure. You MUST call script with "-D" to actually delete : CLI and Jenkins
+MAX_DAYS  = 30      # Delete files older than this value : CLI and Jenkins
 
 # This is what we process
 BASE_PATH = 'http://artifactory.bullhorn.com:8081/artifactory/api/storage'
@@ -31,14 +32,15 @@ DEV_PATH  = BASE_PATH + '/npm-dev'
 REL_PATH  = BASE_PATH + '/npm-release'
 
 # Misc files generated
-LOG_FILE     = 'log'
-DEV_CATALOG  = "dev_catalog.txt"
-REL_CATALOG  = "release_catalog.txt"
-KEEP_FILES   = 'keepers.txt'
-DELETE_FILES = 'deleters.txt'
-SKIPPED_FILES = 'skipped.txt'
+LOG_FILE     = 'log'                # timestamp and ".txt" are appended to this
+DEV_CATALOG  = "dev_catalog.txt"    # Where I store npm-dev results
+REL_CATALOG  = "release_catalog.txt"# Where I store npm-releas results
+KEEP_FILES   = 'keepers.txt'        # Where I store files to keep
+DELETE_FILES = 'deleters.txt'       # Where I store files to delete
+SKIPPED_FILES = 'skipped.txt'       # Where I store files/folders to skip
 
 # Skip the following folders in the npm-dev repo
+# This list can be appended to via the "-S <list,of,folders>" option (comma seperated)
 SKIP_LIST    = ['.npm/@bullhorn-internal',
                 '.npm/@bullhorn',
                 '.npm/bh-elements',
@@ -55,7 +57,8 @@ SKIP_LIST    = ['.npm/@bullhorn-internal',
                 'generator-novo'
                ]
 
-# Skip files that have this in the name
+# Skip files that have this in the name. For now I skip variants of "DO_NOT_DELETE" in the file name
+# This list can only be modified here (for now).
 DO_NOT_DEL_LIST = ['DONOTDELETE',
                    'DO_NOT_DELETE',
                    'DONTDELETE',
@@ -70,17 +73,15 @@ todays_date     = tmp.strptime(todays_date_str, "%Y-%m-%d")
 skipped = list()
 
 def collect_data(uri):
-    """
-    Collect URI data via curl and return JSON output in dict.
-    We must escape the string before we issue 'curli' since file names may contain special characters
-    i.e 'file name (1).txt'  (spaces, parens, etc)
-    """
+    """ Collect URI data via curl and return output in a dict.  """
 
     tmp_file = './.tempfile.txt'
 
     data = list()
-    curl_str = 'curl "' + uri + '" -o ' + tmp_file + " > /dev/null 2>&1"
 
+    # Some file names have spaces and/or parenthesis and don't seem to play well with curl. I have tried escaping the
+    # string with no luck. For now, a simpler solution is to wrap the uri in quotes. So I do that here
+    curl_str = 'curl "' + uri + '" -o ' + tmp_file + " > /dev/null 2>&1"
     lprint ('Processing: %s' % uri, False)
 
     stat = os.system(curl_str)
@@ -89,10 +90,12 @@ def collect_data(uri):
         lprint (msg, True)
         return data
 
+    # This is what I get with those files with spaces, etc
     if os.stat(tmp_file).st_size == 0:
         lprint ('* Warning: curl returned no output for %s' % uri, True)
         return data
 
+    # Curl appears to have run so read the output and store in a dict
     try:
         with open(tmp_file) as file_in:
             data = json.load(file_in)
@@ -103,9 +106,11 @@ def collect_data(uri):
     return data
 
 def traverse(repo_name, data, catalog):
-    """Traverse through folders looking for files. Recuersively calls itself until a folder has been processed"""
+    """ Recursively traverse through folders looking for files. """
+
     global skipped
 
+    # Determine which base path to use
     if repo_name == 'dev':
         use_repo = DEV_PATH
     elif repo_name == 'rel':
@@ -114,36 +119,47 @@ def traverse(repo_name, data, catalog):
         lprint ('* Warning: invalid repo name (%s)' % repo_name, True)
         return catalog
 
-    for c in data['children']:  # Follow the children folders
+    # If the data has a 'children' key then I need to process it further
+    for c in data['children']:
 
-        # Here we skip over entries in the SKIP_LIST. Since that only applies to 'dev' (we want a full record of 'rel'
-        # we only do this when processing 'dev')
+        # If processing the 'DEV' repo, check to see if this is a folder in the SKIP_LIST
         if repo_name == 'dev':
             if len(SKIP_LIST) > 0:      # Make sure there is something to skip
-                if re.findall(r"(?=("+'|'.join(SKIP_LIST)+r"))", data['path']): # See folder to skip is this folder, continue
+
+                # See if this folder (data['path']) is in our list of folders to skip (SKIP_LIST)
+                # If so, skip it by returning
+                if re.findall(r"(?=("+'|'.join(SKIP_LIST)+r"))", data['path']):
                     lprint ('! skipping: %s' % data['uri'], False)
                     skipped.append('Skip Folder: ' + data['uri'])
-                    return(catalog)              # There was a match so return w/o further processing
+                    return(catalog)     # There was a match so return w/o further processing
 
-        # If we are here, we have a folder to process.
-        # Create the full path (new_path) with child name in case this is a folder we need to go deepeer into
+        # If here, this is a valid file/folder to process
+        # Create the full path (new_path) with child name. If it has a 'folder' key we traverse deeper. If not, this
+        # must be a file and I obtain the creation date from the <new_path>
         new_path = use_repo + data['path'] + c['uri']
 #        new_path = data['uri'] + c['uri']
-        new_data = collect_data(new_path)           # Run the curl on the new path
+        new_data = collect_data(new_path)               # Get data on new path
+
+        # If nothing is returned that's an issue. Add it to the 'skip' list and log it as needing "Attention"
         if new_data == None:
             lprint ('! skip NULL dict: %s' % c['uri'], False)
             skipped.append('Attention: ' + data['uri'] + c['uri'])
         else:
-            if c['folder']:                             # If the child is a folder, traverse it
-                traverse(repo_name, new_data, catalog)  # Call myself with new path
+            if c['folder']:                             # If the child is a folder
+                traverse(repo_name, new_data, catalog)  # traverse to the next level
             else:                                       # If not a folder, new_data contains the date info we need
+
                 # If a file name contans "DO_NOT_DELETE" (or some variant thereof) skip it
                 if re.findall(r"(?=("+'|'.join(DO_NOT_DEL_LIST)+r"))", c['uri']):
-                    lprint ('! skipping: %s' % c['uri'], True)
-                    skipped.append('Skip File: ' + data['uri'] + c['uri'])  # Full path
+                    lprint ('! skipping: %s' % c['uri'], True)              # Show file (only)
+                    skipped.append('Skip File: ' + data['uri'] + c['uri'])  # Save full path
                     return(catalog)
 
-                file = data['path'] + c['uri']          # File, relatiuve to BASE
+                file = data['path'] + c['uri']          # File, relative to BASE
+
+                # In some cases curl is returning no date info and I haven't been able to figure out why. It seems
+                # to occur on files with spaces and/or parenthesis in the file name. In any case, until I figure it
+                # out, I will mark the file as 'skipped'.
                 if len(new_data) == 0:
                     msg = '! ignore: no date info found for: ' + data['uri'] + c['uri']
                     lprint (msg, False)
@@ -154,7 +170,7 @@ def traverse(repo_name, data, catalog):
     return(catalog)
 
 def read_data(file):
-    """Read the outut of a real run. Each line is a K|V pair to repopulate the dicts"""
+    """ Read the outut of a real run. Each line is a K|V pair to repopulate the dicts """
     data = list()
     dct  = dict()
 
@@ -211,28 +227,67 @@ def delete_files(lst, u, p):
     """Delete the files from the delete list.
         See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes   for return codes
     """
+    user_skip = False
+
     for f in lst:
         file = DEV_PATH + f
 
         if DO_DELETE:
-            lprint ('deleteing "%s"' % file, False)
-            resp = requests.delete(file, auth=(u, p))
+            lprint('  formatting path..', False)
+
+            # To actually delete the file we must reformat the path we aquired and remove the string '/api/storage'
+            # from the path. If we do not do this calls to delete will return "400" (bad request).
+            file = file.replace('/api/storage', '')
+
+            if INTERACTIVE:
+                user_skip = False
+                lprint ('%s will be deleted next' % file, False)
+                ans = raw_input('Ok to delete [y/n/q]: ')
+                if 'y' in ans:
+                    lprint ('deleteing "%s"' % file, False)
+                    resp = requests.get(file, auth=(u, p))
+#                    resp = requests.delete(file, auth=(u, p))
+                elif 'q' in ans:
+                    return
+                else:
+                    lprint ('skipping "%s"' % file, False)
+                    user_skip = True
+            elif DELETE_ONE:
+                lprint ('DELETE_ONE is set .. exitting', False)
+                resp = requests.get(file, auth=(u, p))
+#                resp = requests.delete(file, auth=(u, p))
+                return
+            else:
+                lprint ('deleteing "%s"' % file, False)
+                resp = requests.get(file, auth=(u, p))
         else:
             lprint ('"get" "%s"' % file, False)
             resp = requests.get(file, auth=(u, p))
 
-        if not resp.status_code <= 200 <= 299:  # Success values (200-299)
-            lprint ('* Warning: %s' % resp, False)
-            lprint ('  a non-success value was returned!', True)
+        print 'user skip: ', user_skip
+        if user_skip == False:
+            if not 200 <= resp.status_code <= 299:  # Success values (200-299)
+                lprint ('* Warning: %s' % resp, False)
+                lprint ('  a non-success value was returned!', True)
+            else:
+                lprint ('request status returned: %d' % resp.status_code, True)
 
 def parse_options():
     """Parse options that are set in the environment (from Jenkins)"""
 
-    global VERBOSE, SKIP_LIST, MAX_DAYS, CLEAN
+    global VERBOSE, SKIP_LIST, MAX_DAYS, CLEAN, DO_DELETE, DELETE_ONE
 
     tmp = os.getenv("VERBOSE")
     if tmp and tmp.lower() in ['true', '1']:
         VERBOSE = True
+
+    tmp = os.getenv("DELETE_ONE")               # Used as a test. Run scans but exit after deleting one file
+    if tmp and tmp.lower() in ['true', '1']:
+        DELETE_ONE = True
+
+    tmp = os.getenv("DO_DELETE")               # Used as a test. Run scans but exit after deleting one file
+    if tmp and tmp.lower() in ['true', '1']:
+        DO_DELETE = True
 
     tmp = os.getenv("KEEP_FILES")
     if tmp and tmp.lower() in ['true', '1']:
@@ -249,7 +304,6 @@ def parse_options():
 def cleanup_temp_files():
     """Clean up temp files"""
 
-#    files = ['./.tempfile.txt']
     files = ['./.tempfile.txt', KEEP_FILES, DELETE_FILES, SKIPPED_FILES, DEV_CATALOG, REL_CATALOG]
 
     for f in files:
@@ -288,7 +342,7 @@ def lprint(msg, wait):
 def main():
     """Main loop of script"""
 
-    global MAX_DAYS, VERBOSE, TEST_MODE, GEN_SAVED_DATA, SKIP_LIST, WAIT, DO_DELETE
+    global MAX_DAYS, VERBOSE, INTERACTIVE, GEN_SAVED_DATA, SKIP_LIST, WAIT, DO_DELETE
     global skipped
 
     rel_catalog = dict()
@@ -297,14 +351,14 @@ def main():
     delete = list()
     skipped = []
 
-    # These are only used for running on CLI. Jenkins passes all (except creds) via os env-vars
+    # These are only used for running on CLI. Jenkins passes its params (except creds) via env-vars in OS
     parser = argparse.ArgumentParser(description='NPM artifact cleaner')
     parser.add_argument('-d', '--days', help='Remove files older than this value', type=int)
     parser.add_argument('-c', '--clean', help='Dont cleanup temp files', action='store_true')
-    parser.add_argument('-q', '--quick', help='Quick test (Done create dlete_list file', action='store_true')
+    parser.add_argument('-o', '--delete_one', help='Delete one file and exit', action='store_true')
+    parser.add_argument('-i', '--interactive', help='User confrims each delete', action='store_true')
     parser.add_argument('-v', '--verbose', help='Be verbose in processing', action='store_true')
     parser.add_argument('-g', '--generate', help='Dont generate saved data files', action='store_true')
-    parser.add_argument('-t', '--test_mode', help='Use saved data and dont execute delete', action='store_true')
     parser.add_argument('-w', '--wait', help='Wait for user (should only be used on CLI)', action='store_true')
     parser.add_argument('-D', '--delete', help='Set this flag to actually delete the files', action='store_true')
     parser.add_argument('-S', '--skip', help='Comma seperated list of folders to add to internal SKIP_LIST', type=str)
@@ -317,14 +371,16 @@ def main():
 
     if args.days:
         MAX_DAYS = args.days
-        if TEST_MODE:
-            print 'TEST_MODE is set and you issued the "-d DAYS" option!'
-            print 'TEST_MODE reads saved data calculated at 30 days, so this might not give the results you expect!'
 
     # If we have CLI options we must set an env-var to be consistent.
     # options not set as env-var are options Jenkins doesn't have
-    if args.test_mode:
-        TEST_MODE = True
+    if args.interactive:
+        INTERACTIVE = True  # Confirm each delete
+        VERBOSE = True      # We have to see what we're doing
+        WAIT = True         # Wait for user input
+        CLEAN = False       # Sont delete files
+    if args.delete_one:
+        os.environ["DELETE_ONE"] = "1"
     if args.delete:
         os.environ["DO_DELETE"] = "1"
     if args.verbose:
@@ -346,6 +402,8 @@ def main():
 
     if DO_DELETE:
         lprint ('** Delete option is set **', True)
+        if DELETE_ONE:
+            lprint ('** Delete one file and exit is also set **', False)
 
     # I could process the data w/o saving it but the data is useful for debugging and running multiple time
     # without having to constantly send requests to artifactory
